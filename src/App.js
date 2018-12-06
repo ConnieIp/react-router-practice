805026d42ab8c79762c0f0a54c59fded8f8de754
Connie Ip <connie.ip@cargosmart.com>
2018-12-06 14:16:19 +0800

    add users,news, not found page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {Route, Link,Switch} from 'react-router-dom'
+import AboutPage from './components/AboutPage.js'
+import NotFoundPage from './components/NotFoundPage.js'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-          <div>
-            <ul>
-              <li>
-                <Link to = "/about">about</Link>
-              </li>
-            </ul>
-          <Route path="/about" component={()=><h2>About</h2>}></Route>
-          </div>
-        </Router>
+        <nav>
+          <ul>
+            <li><Link to = "/about">about</Link></li>
+            <li><Link to = "/users">users</Link></li>
+            <li><Link to = "/news">about</Link></li>
+          </ul>
+        </nav>
+        <hr/>
+        <Switch>
+          <Route path="/" exact component={()=><h1>home page</h1>}></Route>
+          <Route path="/about" component={AboutPage}></Route>
+          <Route path="/users" exact component={()=><h1>users page</h1>}></Route>
+          <Route path="/news" exact component={()=><h1>news page</h1>}></Route>
+          <Route component={NotFoundPage}></Route>
+        </Switch>
       </div>
     );
   }
