2d1d5fcc388139bd62503c11acaef9a11b8172ae
Connie Ip <connie.ip@cargosmart.com>
2018-12-06 14:49:21 +0800

    go back

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 export default class AboutPage extends Component {
+    onClick =() =>{
+        const {history} = this.props
+        history.goBack()
+        // history.go(-1)
+        // history.goForward()
+    }
   render() {
     return (
       <div>
         <h2>About</h2>
+        <button onClick={this.onClick}>go back</button>
       </div>
     )
   }
