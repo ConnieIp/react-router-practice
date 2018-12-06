1fccbf2f815203cd240716d8303ae16ca5cc9423
Connie Ip <connie.ip@cargosmart.com>
2018-12-06 14:36:14 +0800

    modify typo

--- src/App.js
+++ src/App.js
@@ -13,14 +13,14 @@ class App extends Component {
           <ul>
             <li><Link to = "/about">about</Link></li>
             <li><Link to = "/users">users</Link></li>
-            <li><Link to = "/news">about</Link></li>
+            <li><Link to = "/news">news</Link></li>
           </ul>
         </nav>
         <hr/>
         <Switch>
           <Route path="/" exact component={()=><h1>home page</h1>}></Route>
           <Route path="/about" component={AboutPage}></Route>
-          <Route path="/users/:userid" component={({match})=>{return <h1>hello user{match.params.userid}</h1>}}></Route>
+          <Route path="/users/:userid" component={({match})=>{return <h1>hello user{match.params.userid}</h1>}}></Route>{/*cannot write before /users   param is a object 'props', to get userid need props.match.userid*/}
           <Route path="/users" component={()=><h1>users page</h1>}></Route>
           <Route path="/news" component={()=><h1>news page</h1>}></Route>
           <Route component={NotFoundPage}></Route>
